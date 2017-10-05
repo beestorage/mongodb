a74a461d20f3f5a7c9d34bfcb514fdb57c2d09ac
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2017-10-05 16:01:07 +0700

    issuse len to [0] test

--- tc_mongodb/storages/mongo_storage.py
+++ tc_mongodb/storages/mongo_storage.py
@@ -38,7 +38,7 @@ class Storage(BaseStorage):
         storageFsFile = db['fs.files']
         stored = storage.find({'path': path})
 
-        if stored.count() > 0:
+        if stored[0]]:
             docChunk = storageFsChunks.find({'files_id': stored[0]['file_id']})
 
         doc = {
@@ -59,7 +59,7 @@ class Storage(BaseStorage):
         storage.insert(doc_with_crypto)
 
 
-        if len(docChunk) != 0:
+        if docChunk[0]:
             for docC in docChunk:
                 storageFsChunks.delete_many({'_id': docC['_id']})
             docGridFSfile={
