316c509601d88c771739e0ad4b0a47caffcc8f06
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2017-10-11 14:55:12 +0700

    add cp mongostorage

--- setup_mongo.sh
+++ setup_mongo.sh
@@ -1,4 +1,4 @@
 easy_install /usr/src/app/tc_mongodb/
 mv /usr/src/app/tc_mongodb/tc_mongodb /usr/local/lib/python2.7/site-packages/
-#cp /usr/local/lib/python2.7/site-packages/tc_mongodb/storages/mongo_storage.py /usr/local/lib/python2.7/site-packages/thumbor/storages/mongo_storage.py
+cp /usr/local/lib/python2.7/site-packages/tc_mongodb/storages/mongo_storage.py /usr/local/lib/python2.7/site-packages/thumbor/storages/mongo_storage.py
 return 0
