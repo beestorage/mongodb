7f90c81f1416f0048c2ec2fbf82554372a2dca3e
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-02-22 23:41:19 +0700

    clean...

--- tc_mongodb/storages/mongo_storage.py
+++ tc_mongodb/storages/mongo_storage.py
@@ -24,22 +24,22 @@ class Storage(BaseStorage):
         )
 
         db = connection[self.context.config.MONGO_STORAGE_SERVER_DB]
-        storage = db[self.context.config.MONGO_STORAGE_SERVER_COLLECTION]
+        dictThumborToMongo = db[self.context.config.MONGO_STORAGE_SERVER_COLLECTION]
 
-        return connection, db, storage
+        return connection, db, dictThumborToMongo
 
     def put(self, path, bytes):
-        connection, db, storage = self.__conn__()
+        connection, db, dictThumborToMongo = self.__conn__()
 
-        stored=[]
-        docChunk=[]
+        dictData=[]
+        mongoBinaryData=[]
         #Before my Upload https://docs.mongodb.com/manual/core/gridfs/
-        storageFsChunks = db['fs.chunks']
-        storageFsFile = db['fs.files']
-        stored = storage.find_one({'path': path})
+        mongoBinaryStorage = db['fs.chunks']
+        mongoFileMetadata = db['fs.files']
+        dictData = dictThumborToMongo.find_one({'path': path})
 
-        if stored:
-            docChunk = storageFsChunks.find({'files_id': stored['file_id']})
+        if dictData:
+            mongoBinaryData = mongoBinaryStorage.find({'files_id': dictData['file_id']})
 
         doc = {
             'path': path,
@@ -55,23 +55,24 @@ class Storage(BaseStorage):
             doc_with_crypto['crypto'] = self.context.server.security_key
 
         fs = gridfs.GridFS(db)
-        file_data = fs.put(StringIO(bytes), **doc)
+        file_data_id = fs.put(StringIO(bytes), **doc)
 
-        doc_with_crypto['file_id'] = file_data
-        storage.insert(doc_with_crypto)
+        doc_with_crypto['file_id'] = file_data_id
+        dictThumborToMongo.insert(doc_with_crypto)
 
 
-        if stored:
-            for docC in docChunk:
-                storageFsChunks.delete_many({'_id': docC['_id']})
-            docGridFSfile={
-                '_id': stored['file_id']
+        if dictData:
+            for docC in mongoBinaryData:
+                mongoBinaryStorage.delete_many({'_id': docC['_id']})
+            mongoGridFsQuery={
+                '_id': dictData['file_id']
             }
-            docCollremove = {
-                '_id': stored['_id']
+            mongoDictQuery = {
+                '_id': dictData['_id']
             }
-            storage.delete_many(docCollremove)
-            storageFsFile.delete_many(docGridFSfile)
+            dictThumborToMongo.delete_many(mongoDictQuery)
+            mongoFileMetadata.delete_many(mongoGridFsQuery)
+            # TODO check remove binary file
 
         return path
 
@@ -79,87 +80,87 @@ class Storage(BaseStorage):
         if not self.context.config.STORES_CRYPTO_KEY_FOR_EACH_IMAGE:
             return
 
-        connection, db, storage = self.__conn__()
+        connection, db, dictThumborToMongo = self.__conn__()
 
         if not self.context.server.security_key:
             raise RuntimeError("STORES_CRYPTO_KEY_FOR_EACH_IMAGE can't be True if no SECURITY_KEY specified")
 
-        crypto = storage.find_one({'path': path})
+        crypto = dictThumborToMongo.find_one({'path': path})
 
         crypto['crypto'] = self.context.server.security_key
-        storage.update({'path': path}, crypto)
+        dictThumborToMongo.update({'path': path}, crypto)
         return path
 
     def put_detector_data(self, path, data):
-        connection, db, storage = self.__conn__()
+        connection, db, dictThumborToMongo = self.__conn__()
 
-        storage.update({'path': path}, {"$set": {"detector_data": data}})
+        dictThumborToMongo.update({'path': path}, {"$set": {"detector_data": data}})
         return path
 
     @return_future
     def get_crypto(self, path, callback):
-        connection, db, storage = self.__conn__()
+        connection, db, dictThumborToMongo = self.__conn__()
 
-        crypto = storage.find_one({'path': path})
+        crypto = dictThumborToMongo.find_one({'path': path})
         callback(crypto.get('crypto') if crypto else None)
 
     @return_future
     def get_detector_data(self, path, callback):
-        connection, db, storage = self.__conn__()
+        connection, db, dictThumborToMongo = self.__conn__()
 
-        doc = storage.find_one({'path': path})
+        doc = dictThumborToMongo.find_one({'path': path})
         callback(doc.get('detector_data') if doc else None)
 
     @return_future
     def get(self, path, callback):
-        connection, db, storage = self.__conn__()
+        connection, db, dictThumborToMongo = self.__conn__()
 
-        stored = storage.find_one({'path': path})
+        dictData = dictThumborToMongo.find_one({'path': path})
 
-        if not stored or self.__is_expired(stored):
+        if not dictData or self.__is_expired(dictData):
             callback(None)
             return
 
         fs = gridfs.GridFS(db)
 
-        contents = fs.get(stored['file_id']).read()
+        contents = fs.get(dictData['file_id']).read()
 
         callback(str(contents))
 
     @return_future
     def exists(self, path, callback):
-        connection, db, storage = self.__conn__()
+        connection, db, dictThumborToMongo = self.__conn__()
 
-        stored = storage.find_one({'path': path})
+        dictData = dictThumborToMongo.find_one({'path': path})
 
-        if not stored or self.__is_expired(stored):
+        if not dictData or self.__is_expired(dictData):
             callback(False)
         else:
             callback(True)
 
     def remove(self, path):
-        connection, db, storage = self.__conn__()
+        connection, db, dictThumborToMongo = self.__conn__()
 
-        storageFsFile = db['fs.files']
-        storageFsChunks = db['fs.chunks']
+        mongoFileMetadata = db['fs.files']
+        mongoBinaryStorage = db['fs.chunks']
 
         #Q find id
-        stored = storage.find_one({'path': path})
+        dictData = dictThumborToMongo.find_one({'path': path})
 
         docGridFSchunks = {
-            'files_id': stored['file_id']
+            'files_id': dictData['file_id']
         }
-        docGridFSfile={
-            '_id': stored['file_id']
+        mongoGridFsQuery={
+            '_id': dictData['file_id']
         }
-        docCollremove = {
+        mongoDictQuery = {
             'path': path
         }
-        storage.delete_many(docCollremove)
-        storageFsFile.delete_many(docGridFSfile)
-        storageFsChunks.delete_many(docGridFSchunks)
+        dictThumborToMongo.delete_many(mongoDictQuery)
+        mongoFileMetadata.delete_many(mongoGridFsQuery)
+        mongoBinaryStorage.delete_many(docGridFSchunks)
 
-    def __is_expired(self, stored):
-        timediff = datetime.now() - stored.get('created_at')
+    def __is_expired(self, dictData):
+        timediff = datetime.now() - dictData.get('created_at')
         return timediff > timedelta(seconds=self.context.config.STORAGE_EXPIRATION_SECONDS)
         #return False
